e261f9d550f6ac41cc2293ed458ab44353f724ed
UjjwalAgarwal0575 <agarwal.ujjwal02@gmail.com>
2022-11-25 12:33:05 +0530

    "Updated functions"

--- Test.py
+++ Test.py
@@ -5,42 +5,42 @@ import unittest
 from Prog1 import mul
 
 class TestSum(unittest.TestCase):
-    def test_list_int(self):
+    def test_list_int1(self):
         """
         Test case to add two numbers
         """
         data = [2, 3]
         result = mul(data)
         self.assertEqual(result, 6)
-    def test_list_int(self):
+    def test_list_int2(self):
         """
         Test case to add two numbers
         """
         data = [0, 30]
         result = mul(data)
         self.assertEqual(result, 0)
-    def test_list_int(self):
+    def test_list_int3(self):
         """
         Test case to add two numbers
         """
         data = [2,1]
         result = mul(data)
         self.assertEqual(result, 2)
-    def test_list_int(self):
+    def test_list_int4(self):
         """
         Test case to add two numbers
         """
         data = [2, 3]
         result = mul(data)
         self.assertEqual(result, 1)
-    def test_list_int(self):
+    def test_list_int5(self):
         """
         Test case to add two numbers
         """
         data = [0, 30]
         result = mul(data)
         self.assertEqual(result, 30)
-    def test_list_int(self):
+    def test_list_int6(self):
         """
         Test case to add two numbers
         """
